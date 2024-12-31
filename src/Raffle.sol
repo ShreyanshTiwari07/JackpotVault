3a0ce3a799b099daf2aec2662f161c68f40fae14
root <root@Shreyansh>
2025-01-01 04:46:30 +0530

    Fix: Format code using forge fmt

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,39 +1,33 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
+contract Raffle {
+    error Raffle_insufficientAmount();
 
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
 
-contract Raffle{
+    event RaffleEntered(address indexed player);
 
- error Raffle_insufficientAmount();
+    constructor(uint256 entranceFee, uint256 interval) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+    }
 
-uint256 private immutable i_entranceFee;
-address payable[] private s_players;
-uint256 private immutable i_interval;
-uint256 private s_lastTimeStamp;
+    function enterRaffle() external payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle_insufficientAmount();
+        }
+        s_players.push(payable(msg.sender)); //whenever updating storage variable-> emit an event
+        emit RaffleEntered(msg.sender);
+    }
 
-event RaffleEntered(address indexed player);
+    function pickWinner() external {}
 
-constructor (uint256 entranceFee, uint256 interval){
-    i_entranceFee=entranceFee;
-    i_interval=interval;
-    s_lastTimeStamp= block.timestamp;
-}
-
-function enterRaffle()external payable {
-if(msg.value<i_entranceFee){
-    revert Raffle_insufficientAmount();
-}
-s_players.push(payable(msg.sender)); //whenever updating storage variable-> emit an event
-emit RaffleEntered(msg.sender);
-}
-
-function pickWinner()external{
-
-}
-
-function getEntryFee()public view returns(uint256){
-    return i_entranceFee;
-}
-
+    function getEntryFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
 }
